d8dcc59839bc0af35395d43e39786c3896230b4b
shrugsimontiger <152566433+shrugsimontiger@users.noreply.github.com>
2023-12-13 22:39:12 -0500

    Added MUCH more template code I can use depending on the puzzle

--- template.js
+++ template.js
@@ -1,2 +1,15 @@
-const { readFileSync } = require("fs");
-const lines = readFileSync("input.txt", "utf-8").split("\n");
+const { readFileSync } = require("fs");                                                                                                          // Base import statement
+const lines = readFileSync("input.txt", "utf-8").split("\n");                                                                                    // List of lines
+const grid = readFileSync("input.txt", "utf-8").split("\n");                                                                                     // Grid
+const grid = readFileSync("input.txt", "utf-8").split("\n").map(row => row.split(""));                                                           // Mutable grid
+const paras = readFileSync("input.txt", "utf-8").split("\n\n").map(para => para.split("\n"));                                                    // Paragraphs of lines
+const grids = readFileSync("input.txt", "utf-8").split("\n\n").map(grid => grid.split("\n"));                                                    // Multiple grids
+const grids = readFileSync("input.txt", "utf-8").split("\n\n").map(grid => grid.split("\n").map(row => row.split("\n")));                        // Multiple mutable grids
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(" "));                                                       // Lines of space-separated values
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(", "));                                                      // Lines of comma-separated values
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(": ")[0].split(", "));                                       // Lines of comma-separated values with headers
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(" ").map(x => parseInt(x)));                                 // Lines of space-separated integers
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(", ").map(x => parseInt(x)));                                // Lines of comma-separated integers
+const lines = readFileSync("input.txt", "utf-8").split("\n").map(line => line.split(": ")[0].split(", ").map(x => parseInt(x)));                 // Lines of comma-separated integers with headers
+const paras = readFileSync("input.txt", "utf-8").split("\n\n").map(para => para.split("\n").map(line => line.split(" ")));                       // Paragraphs of lines of space-separated values
+const paras = readFileSync("input.txt", "utf-8").split("\n\n").map(para => para.split("\n").map(line => line.split(" ")).map(x => parseInt(x))); // Paragraphs of lines of space-separated integers
